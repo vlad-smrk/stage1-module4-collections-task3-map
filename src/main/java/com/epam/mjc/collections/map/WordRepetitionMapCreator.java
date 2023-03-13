2edccc9982687472158ba2ce44fcb7f32539b51e
Vladislav Samorukov <smrk.vld@gmail.com>
2023-03-14 01:59:49 +0300

    WordRepetition empty sentence fixed

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -2,10 +2,16 @@ package com.epam.mjc.collections.map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+
         String[] words = sentence.split("\\W+");
+        if (Objects.equals(words[0], "")) {
+            return new HashMap<>();
+        }
+
         Map<String, Integer> result = new HashMap<>();
         for (String word : words) {
             word = word.toLowerCase();
@@ -13,5 +19,10 @@ public class WordRepetitionMapCreator {
             result.put(word, count + 1);
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> test = new WordRepetitionMapCreator().createWordRepetitionMap("");
+        System.out.println(test);
     }
 }
